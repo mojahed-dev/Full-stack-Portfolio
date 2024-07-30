bd2087b638bd4f42c8f68a7e323826d68daaad87
MOJAHED.ALO.HABIB <mojahed.alo.habib@gmail.com>
2024-07-31 00:53:14 +0800

    modify: footer

--- client/src/pages/Home/Footer.js
+++ client/src/pages/Home/Footer.js
@@ -6,10 +6,10 @@ const Footer = () => {
             <div className='h-[1px] w-full bg-gray-700'>
             </div>
             <div className="flex items-center justify-center flex-col mt-10 opacity-70">
-                <p className="text-white">
+                <p>
                     Designed and Developed By
                 </p>
-                <p className="text-white">Mojahed Habib</p>
+                <p>Mojahed Habib</p>
             </div>
         </div>
     )
