2de470572dcb7da987f27200591ac9eb828c9139
MOJAHED.ALO.HABIB <mojahed.alo.habib@gmail.com>
2024-07-31 03:55:56 +0800

    modify: updates

--- client/src/pages/Home/Project.js
+++ client/src/pages/Home/Project.js
@@ -44,12 +44,12 @@ const Project = () => {
                     </div>
                 </div>
             </div> */}
-            <div className='flex gap-10 items-center justify-center sm:flex-col'>
+            <div className='flex gap-10 items-center justify-center sm:flex-col lg:grid lg:grid-cols-4'>
                 {project.map((pro, index) => (
                     <div className="card bg-base-100 w-96 shadow-xl z-0">
                         <figure>
                             <img
-                                src={(pro.image) ? pro.image : 'https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp'}
+                                src={(pro.image) ? pro.image : ''}
                                 alt="Project" />
                         </figure>
                         <div className="card-body">
